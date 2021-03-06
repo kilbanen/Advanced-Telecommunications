ead9b54c99fc115a6cb4954376662dec1b300efc
Kilbane <kilbanen@tcd.ie>
2021-03-06 20:19:01 +0000

    Got HTTPS working

--- Project_1/server.py
+++ Project_1/server.py
@@ -7,11 +7,7 @@ MAX_DATA_SIZE = 8192
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-
-# Create socket
-incoming = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-incoming.bind(ADDR)
+FORMAT = 'ISO-8859-1'
 
 # Client handler
 def handle_client(conn, addr):
@@ -25,30 +21,48 @@ def handle_client(conn, addr):
         msg = request.decode(FORMAT)
         print(f"[{addr}] {msg}")
         # Extract the webserver and port from the message
-        webserver, port = get_host(msg)
-        # Create outgoing socket and send request
-        print(f"Sending request to {webserver} {port}")
+        method, webserver, port = parse_message(msg)
         outgoing = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         outgoing.connect((webserver, port))
-        outgoing.sendall(request)
-        while connected:
-            response = outgoing.recv(MAX_DATA_SIZE)
-            #msg = response.decode(FORMAT)
-            print(f"Received response {response}")
-            if (len(response) > 0):
-                print("Sending response to client")
-                conn.send(response)
-            else:
-                connected = False
+        # Create outgoing socket and send request
+        print(f"Sending {method} request to {webserver} {port}")
+        if(method == "CONNECT"):
+            response = "HTTP/1.1 200 Connection established\r\nProxy-agent: Proxy\r\n\r\n".encode()
+            print("Initiating connection...")
+            conn.sendall(response)
+            conn.setblocking(0)
+            outgoing.setblocking(0)
+            while True:
+                try:
+                    request = conn.recv(MAX_DATA_SIZE)
+                    outgoing.sendall(request)
+                except socket.error as e:
+                    pass
+                try:
+                    response = outgoing.recv(MAX_DATA_SIZE)
+                    conn.sendall(response)
+                except socket.error as e:
+                    pass
+            connected = False
+        else:
+            outgoing.sendall(request)
+            while connected:
+                response = outgoing.recv(MAX_DATA_SIZE)
+                print(f"Received response {response}")
+                if (len(response) > 0):
+                    print("Sending response to client")
+                    conn.send(response)
+                else:
+                    connected = False
     outgoing.close()
     conn.close()
 
-def get_host(message):
+def parse_message(message):
     # Default port
     port = 80
     first_line = message.split('\n')[0]
-    message_type = first_line.split(' ')[0]
-    if message_type == 'CONNECT':
+    method = first_line.split(' ')[0]
+    if method == 'CONNECT':
         host_line = message.split('\n')[4]
     else:
         host_line = message.split('\n')[1]
@@ -58,9 +72,12 @@ def get_host(message):
     # Specific port
         port = int(host[(port_pos+1):])
     webserver = host[:port_pos]
-    return webserver, port
+    return method, webserver, port
 
 def start():
+    # Create socket
+    incoming = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    incoming.bind(ADDR)
     incoming.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
