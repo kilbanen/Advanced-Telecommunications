9773bfc4ad1ea80772513d1bd588dcec05c3fb6f
Kilbane <kilbanen@tcd.ie>
2021-03-08 21:20:39 +0000

    Refactored code

--- Project_1/server.py
+++ Project_1/server.py
@@ -12,48 +12,23 @@ FORMAT = 'ISO-8859-1'
 # Client handler
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
-
-    connected = True
-    while connected:
-        # Receive the request
-        request = conn.recv(MAX_DATA_SIZE)
-        # Convert it to a readable format
-        msg = request.decode(FORMAT)
-        print(f"[{addr}] {msg}")
-        # Extract the webserver and port from the message
-        method, webserver, port = parse_message(msg)
-        outgoing = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        outgoing.connect((webserver, port))
-        # Create outgoing socket and send request
-        print(f"Sending {method} request to {webserver} {port}")
-        if(method == "CONNECT"):
-            response = "HTTP/1.1 200 Connection established\r\nProxy-agent: Proxy\r\n\r\n".encode()
-            print("Initiating connection...")
-            conn.sendall(response)
-            conn.setblocking(0)
-            outgoing.setblocking(0)
-            while True:
-                try:
-                    request = conn.recv(MAX_DATA_SIZE)
-                    outgoing.sendall(request)
-                except socket.error as e:
-                    pass
-                try:
-                    response = outgoing.recv(MAX_DATA_SIZE)
-                    conn.sendall(response)
-                except socket.error as e:
-                    pass
-            connected = False
-        else:
-            outgoing.sendall(request)
-            while connected:
-                response = outgoing.recv(MAX_DATA_SIZE)
-                print(f"Received response {response}")
-                if (len(response) > 0):
-                    print("Sending response to client")
-                    conn.send(response)
-                else:
-                    connected = False
+    # Receive the request
+    request = conn.recv(MAX_DATA_SIZE)
+    # Convert it to a readable format
+    msg = request.decode(FORMAT)
+    print(f"[{addr}] {msg}")
+    # Extract the webserver and port from the message
+    method, webserver, port = parse_message(msg)
+    outgoing = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    outgoing.connect((webserver, port))
+    # Create outgoing socket and send request
+    print(f"Sending {method} request to {webserver} {port}")
+    if(method == "CONNECT"):
+        https_connection(conn, outgoing)
+    else:
+        outgoing.sendall(request)
+        http_connection(conn, outgoing)
+    print("Closing connection...")
     outgoing.close()
     conn.close()
 
@@ -62,6 +37,7 @@ def parse_message(message):
     port = 80
     first_line = message.split('\n')[0]
     method = first_line.split(' ')[0]
+    # Check if CONNECT method
     if method == 'CONNECT':
         host_line = message.split('\n')[4]
     else:
@@ -74,13 +50,47 @@ def parse_message(message):
     webserver = host[:port_pos]
     return method, webserver, port
 
+def https_connection(conn, outgoing):
+    ok = "HTTP/1.1 200 Connection established\r\nProxy-agent: Proxy\r\n\r\n".encode()
+    print("Initiating connection...")
+    conn.sendall(ok)
+    conn.setblocking(0)
+    outgoing.setblocking(0)
+    while True:
+        try:
+            request = conn.recv(MAX_DATA_SIZE)
+            outgoing.sendall(request)
+        except socket.error as e:
+            pass
+        try:
+            response = outgoing.recv(MAX_DATA_SIZE)
+            if (len(response) > 0):
+                conn.sendall(response)
+            else:
+                break
+        except socket.error as e:
+            pass
+    return 0
+
+def http_connection(conn, outgoing):
+    while True:
+        response = outgoing.recv(MAX_DATA_SIZE)
+        print(f"Received response {response}")
+        if (len(response) > 0):
+            conn.send(response)
+        else:
+            break
+
 def start():
+    threadnumber = 1
     # Create socket
     incoming = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     incoming.bind(ADDR)
     incoming.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
+        print(f"Starting thread {threadnumber}")
+        threadnumber = threadnumber + 1
         conn, addr = incoming.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
